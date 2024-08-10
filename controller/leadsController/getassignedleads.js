129fe683ecad9272199ff412c55f2b570e4f6965
Nikhil Lende <110834015+Mr-unick@users.noreply.github.com>
2024-08-10 09:53:48 -0700

    Add files via upload

--- controller/leadsController/getassignedleads.js
+++ controller/leadsController/getassignedleads.js
@@ -1,17 +1,19 @@
-const Leads = require("../../db/leadSchema");
+const { Leads } = require("../../db/leadSchema");
+
 
 const GetAssignedLeads = async (req, res) => {
   try {
 
-    const collaboratorId =
-      req.params.collaboratorId 
+    const collaboratorId =req.params.collaboratorId 
+      console.log(collaboratorId,'from controller')
 
-    if (!collaboratorId) {
-      return res.status(400).send({ error: "Collaborator ID is required" });
-    }
+   
     const data = await Leads.find({
-      collaborators: { $elemMatch: { id: collaboratorId } },
+      collaborators: { $elemMatch: { _id: collaboratorId } },
     });
+
+    console.log(data,'hello data');
+    
 
     res.status(200).send(data);
   } catch (e) {
